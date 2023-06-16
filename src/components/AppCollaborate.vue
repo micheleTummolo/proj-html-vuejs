f7cc753d23efd25963869b56338bdd4f64a48fcc
micheleTummolo <tummolomichele02@gmail.com>
2023-06-16 14:55:42 +0200

    test 2 form netlify

--- src/components/AppCollaborate.vue
+++ src/components/AppCollaborate.vue
@@ -17,7 +17,7 @@ export default {
                     lacus imperdiet.</p>
             </div>
             <div class="col-6 my-4 px-4">
-                <form name="contact" method="POST" data-netlify="true">
+                <form name="contact" netlify>
                     <div class="row mb-4">
                         <div class="col-6">
                             <input type="text" name="" id="" placeholder="Your Name">
